429f1739bda92afab5278cbb1c855069cd70cc5a
alchemistOfWeb <nikitakzntcv@gmail.com>
2021-12-16 22:39:30 +0400

    updated readme.md and updated relations between tasks

--- kanban_board_project/main/serializers.py
+++ kanban_board_project/main/serializers.py
@@ -14,32 +14,14 @@ class BoardSerializer(serializers.ModelSerializer):
 
 
 class TaskTagSerializer(serializers.ModelSerializer):
-    """ 
-    fields:
-    {
-        title: Char(255, unique),
-    }
-    """
+    
+    
     class Meta:
         model = TaskTag
         fields = '__all__'
 
 
 class FilteredTaskSerializer(serializers.ListSerializer):
-    """
-    # completed=True/False
-    # &
-
-    deadline_range between date1 and date2[x]
-    &
-    order_by start_at or deadline_at[ ] and order_by completion
-    &
-    has tags in tags_array[x]
-    &
-    is_archive
-    &
-    completion between degree1 and degree2
-    """
     allow_orderby_params = ['completion', '-completion', 'deadline_at', '-deadline_at']
 
 
@@ -114,38 +96,21 @@ class FilteredTaskSerializer(serializers.ListSerializer):
         model = Task
         fields = '__all__'
 
+# class SubtaskSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         fields = ['id']
+
 
 class TaskSerializer(serializers.ModelSerializer):
-    """ 
-    fields:
-    {
-        title: Char(255),
-        start_at: datetime,
-        deadline_at: datetime,
-        completion: float(0-100),
-        board: id,
-        todolist: id,
-        position: integer
-    }  
-
-    todo: normal tags field
-    """
+    blocked_by = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    subtasks = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    
     # tags = TaskTagSerializer(many=True)
     # def get_tags(self):
     #     tags = TaskTagSerializer(many=True)
 
-    # def create(self, validated_data):
-    #     # print("__________________")
-    #     # print(validated_data)
-    #     return super().create(validated_data)
-
-    # blocks = BlockTaskListSerializer(many=True)
-
     # blocked_by = serializers.SerializerMethodField(method_name="get_blocked_by")
 
-    # def get_blocked_by(self, obj):
-    #     obj.blocked_by
-    #     return
     class Meta:
         model = Task
         fields = '__all__'
@@ -153,12 +118,6 @@ class TaskSerializer(serializers.ModelSerializer):
 
 
 class TodoListSerializer(serializers.ModelSerializer):
-    """
-    create,
-    get,
-    update,
-    delete
-    """
 
     class Meta:
         model = TodoList
