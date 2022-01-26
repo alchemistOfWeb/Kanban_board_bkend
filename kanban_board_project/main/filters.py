b907fea2f71277814b3c79883f1f29200dfa5d2e
alchemistOfWeb <nikitakzntcv@gmail.com>
2022-01-27 00:53:03 +0400

    some small fixes in filter.py

--- kanban_board_project/main/filters.py
+++ kanban_board_project/main/filters.py
@@ -8,16 +8,12 @@ class IdFilterInFilter(filters.BaseInFilter, filters.NumberFilter):
 
 
 class TaskFilter(filters.FilterSet):
-    # deadline_at = filters.DateTimeFromToRangeFilter()
-    # deadline_at_after: 2022-03-02 18:00, deadline_at_before: 2022-01-01 8:00
-    # completion = filters.RangeFilter()
-    # completion_min: 90, completion_max: 100
+    # format: Y-M-d[ H[:i[:s]]]
+    dl_min = filters.DateTimeFilter(field_name='deadline_at', lookup_expr='gt')
+    dl_max = filters.DateTimeFilter(field_name='deadline_at', lookup_expr='lt')
 
-    deadline__gt = filters.NumberFilter(field_name='deadline_at', lookup_expr='gt')
-    deadline__lt = filters.NumberFilter(field_name='deadline_at', lookup_expr='lt')
-
-    completion__gt = filters.NumberFilter(field_name='completion', lookup_expr='gt')
-    completion__lt = filters.NumberFilter(field_name='completion', lookup_expr='lt')
+    completion_min = filters.NumberFilter(field_name='completion', lookup_expr='gt')
+    completion_max = filters.NumberFilter(field_name='completion', lookup_expr='lt')
 
     tags = IdFilterInFilter(field_name='tags__id', lookup_expr='in')
 
